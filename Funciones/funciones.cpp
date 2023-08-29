7301f9ef0173b9f6d2208622ad25f48b13bea32f
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-08-29 08:00:00 -0500

    first commit

---
+++ Funciones/funciones.cpp
@@ -0,0 +1,8 @@
+//
+//  funciones.cpp
+//  Funciones
+//
+//  Created by Hope Alvarez on 29/08/23.
+//
+
+#include <stdio.h>
---
+++ Funciones/funciones.hpp
@@ -0,0 +1,13 @@
+//
+//  funciones.hpp
+//  Funciones
+//
+//  Created by Hope Alvarez on 29/08/23.
+//
+
+#ifndef funciones_hpp
+#define funciones_hpp
+
+#include <stdio.h>
+
+#endif /* funciones_hpp */
