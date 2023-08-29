8502ec2a0f7ab8990613054844a10961ba423b21
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-08-29 08:00:14 -0500

    first comit

--- Funciones/main.cpp
+++ Funciones/main.cpp
@@ -1,14 +1,75 @@
-//
-//  main.cpp
-//  Funciones
-//
-//  Created by Hope Alvarez on 29/08/23.
-//
-
+//  Nombre: Álvaro José Álvarez Puello
+#define _USE MATH_DEFINES
 #include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//  Perimetro del circulo
+float perimetroCirculo1 (float radio) {
+    float p;
+    p = 2 * M_PI * radio;
+    
+    return p;
+}
+
+void perimetroCirculo2 (float radio) {
+    float p;
+    p = 2 * M_PI * radio;
+    
+    cout << "El perimetro del circulo 2 es: " << p << endl;
+}
+
+float perimetroCirculo3() {
+    float p, radio;
+    
+    cout << "Ingrese el radio 3: ";
+    cin >> radio;
+    
+    p = 2 * M_PI * radio;
+    
+    return p;
+}
+
+void perimetroCirculo4() {
+    float p, radio;
+    
+    cout << "Ingrese el radio 4: ";
+    cin >> radio;
+    
+    p = 2 * M_PI * radio;
+    
+    cout << "El perimetro del circulo 4 es: " << p << endl;
+}
+
+
+//  Main program
+int main() {
+    cout << fixed << setprecision(3);
+    cout << "Bienvenido al caso de fig. geometricas \nAquí solo calcularemos el perimetro del circulo" << endl;
+    
+    float r, perimetro;
+    
+    cout << endl;
+    cout << "Ingrese el radio 1: ";
+    cin >> r;
+    perimetro = perimetroCirculo1(r);
+    cout << "El perimetro del circulo 1 es: " << perimetro << endl;
+    
+    cout << endl;
+    cout << "Ingrese el radio 2: ";
+    cin >> r;
+    perimetroCirculo2(r);
+    
+    cout << endl;
+    perimetro = perimetroCirculo3();
+    cout << "El perimetro del circulo 3 es: " << perimetro << endl;
+    
+    cout << endl;
+    perimetroCirculo4();
+    
+    cout << endl;
+    cout << "Fin del programa \nBye!" << endl;
+
     return 0;
 }
--- funciones.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  funciones.cpp
-//  Funciones
-//
-//  Created by Hope Alvarez on 29/08/23.
-//
-
-#include <stdio.h>
