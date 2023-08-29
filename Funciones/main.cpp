0deb53542ec545a9cc41b34542e605146feb393e
AlvaNeedsFood <aalvarez.contact05@gmail.com>
2023-08-29 07:02:29 -0500

    Initial Commit

---
+++ Funciones/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Funciones
+//
+//  Created by Hope Alvarez on 29/08/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
